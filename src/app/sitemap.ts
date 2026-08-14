c72899a75599e6504e98ea20856e4260281cc8f7
iafinderbr <iafinderbr@gmail.com>
2026-08-14 18:33:44 -0300

    chore: adiciona loja virtual ao sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -44,5 +44,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+    {
+      url: "https://anunciaai.vercel.app/gerador-de-anuncios-para-loja-virtual",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
