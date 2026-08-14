7aead6440f123bf78a03e5cee0661901ed3112db
iafinderbr <iafinderbr@gmail.com>
2026-08-14 18:06:07 -0300

    Adicionar pagina OLX ao sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -38,5 +38,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+    {
+      url: "https://anunciaai.vercel.app/gerador-de-anuncios-olx",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
