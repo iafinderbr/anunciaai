d96354e42aef511db09987597a272e0e599c647b
iafinderbr <iafinderbr@gmail.com>
2026-08-14 05:25:06 -0300

    Adicionar gerador Shopee ao sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -26,5 +26,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+    {
+      url: "https://anunciaai.vercel.app/gerador-de-anuncios-shopee",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
