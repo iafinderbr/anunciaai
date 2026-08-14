fef9daa0eb752ae2d190c917bf4c0eb4d4baecfe
iafinderbr <iafinderbr@gmail.com>
2026-08-14 05:47:19 -0300

    Adicionar gerador Instagram ao sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -32,5 +32,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+    {
+      url: "https://anunciaai.vercel.app/gerador-de-legendas-para-instagram",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
