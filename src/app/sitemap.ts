7f85de147734b95dd89ad4510e34a7c17ec2aa3f
iafinderbr <iafinderbr@gmail.com>
2026-08-14 04:56:14 -0300

    Adicionar gerador de titulos ao sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,5 +20,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+    {
+      url: "https://anunciaai.vercel.app/gerador-de-titulos-para-produtos",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
